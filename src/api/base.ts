2f91069b94e3a408f91d28632ddcfff557e062f2
bea <beatriztattooer@gmail.com>
2024-08-13 20:19:18 -0300

    :mute: removing logs

--- src/api/base.ts
+++ src/api/base.ts
@@ -1,21 +1,17 @@
+const apiBase = "https://gateway.marvel.com:443/v1/public";
+
 export class API {
-  static readonly apiBase: "https://gateway.marvel.com:443/v1/public";
 
   static async request<T>(endpoint: string, options: RequestInit = {}): Promise<T> {
-    const url = `${this.apiBase}${endpoint}`;
-    
+    const url = `${apiBase}${endpoint}`;
     try {
       const response = await fetch(url, options);
       if (!response.ok) {
         throw new Error(`Error: ${response.status} ${response.statusText}`);
       }
       const data = await response.json();
-      // TODO: remover ao subir o commit:
-      console.log('resultado:', data);
       return data as T;
     } catch (error) {
-      // TODO: remover ao subir o commit:
-      console.log('deu merda:', error);
       throw error;
     }
   }
